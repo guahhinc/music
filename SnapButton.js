239b6da78bc8d88e27479b4cf31816bc2cd7efb3
Guahh <t66305218@gmail.com>
2025-10-22 16:23:14 +1100

    Delete SnapButton.js

--- SnapButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SnapButton() {
-  return (
-    <button className="snap-btn" title="Snap!">
-      <span role="img" aria-label="Snap Camera">📷</span>
-    </button>
-  );
-}
